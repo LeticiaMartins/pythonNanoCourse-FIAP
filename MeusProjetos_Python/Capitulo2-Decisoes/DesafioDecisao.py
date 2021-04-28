6d976a1049932fe97126d9f470abc1feceb94630
LeticiaMartins <leticiamartinscode@gmail.com>
2021-04-27 23:14:46 -0300

    Desafio de decisão com if, else e elif

---
+++ MeusProjetos_Python/Capitulo2-Decisoes/DesafioDecisao.py
@@ -0,0 +1,18 @@
+nivel = input("Digite seu nível de acesso: ").upper()
+
+if nivel == "ADM" or nivel == "USR":
+    genero = input("Digite seu gênero: ").upper()
+    if nivel == "ADM":
+        if genero == "FEMININO":
+            print("Olá administradora")
+        else:
+            print("Olá administrador")
+    else:
+        if genero == "FEMININO":
+            print("Olá usuária")
+        else:
+            print("Olá usuário")
+elif nivel == "GUEST":
+    print("Olá visitante")
+else:
+    print("Olá desonhecido(a)")
