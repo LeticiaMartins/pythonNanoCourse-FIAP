67983afd8d2d03f9caced7a11ccaa5dacd28553f
LeticiaMartins <leticiamartinscode@gmail.com>
2021-04-27 22:08:15 -0300

    Initial commit

---
+++ MeusProjetos_Python/Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,8 @@
+responsavel = input("Digite para quem declarar: ")
+funcionario = input("Digite o nome funcionario: ")
+evento = input("Digite o evento: ")
+valor = float(input("Digite o valor gasto com a entrada: "))
+
+print("Declaro para o senhor " + responsavel + ", que o senhor " + funcionario +
+      " esteve presente no evento " + evento +
+      " e gastou o valor de R$" + str(valor) + " com a entrada.")
