b8410d6134f5cca81a692513821a529005015713
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-06-29 21:39:45 -0300

    final cap 2

---
+++ MeusProjetos_Python/Capitulo2-Repeticoes/DecisaoEncadeada2While.py
@@ -0,0 +1,28 @@
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+doenca_infectocontagiosa = input("Suspeita de doença infectocontagiosa? ").upper()
+
+# Primeiro problema a ser resolvido
+while doenca_infectocontagiosa != "SIM" and doenca_infectocontagiosa != "NAO":
+    print("Digite SIM ou NAO")
+    doenca_infectocontagiosa = input("Suspeita de doença infecto-contagiosa? ").upper()
+
+if doenca_infectocontagiosa == "SIM":
+    print("Encaminhe o paciente para sala AMARELA")
+else:
+    print("Encaminhe o paciente para a sala BRANCA")
+
+# Segundo problema a ser resolvido
+
+if idade >= 65:
+    print("Paciente COM prioridade")
+else:
+    genero = input("Digite o gênero do paciente: ").upper()
+    if genero == "FEMININO" and idade > 10:
+        gravidez = input("A paciente está grávida? ").upper()
+        if gravidez == "SIM":
+            print("Paciente COM prioridade")
+        else:
+            print("Paciente SEM prioridade")
+    else:
+        print("Paciente SEM prioridade")
