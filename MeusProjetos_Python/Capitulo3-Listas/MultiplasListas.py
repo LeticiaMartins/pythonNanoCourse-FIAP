8552abfcff8237917d86be6d87ff097326c81d28
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-01 15:42:14 -0300

    Depreciação de preço para alterar na lista e eliminação de itens da lista

--- MeusProjetos_Python/Capitulo3-Listas/MultiplasListas.py
+++ MeusProjetos_Python/Capitulo3-Listas/MultiplasListas.py
@@ -24,3 +24,31 @@ for indice in range(0, len(equipamentos)):
     if busca == equipamentos[indice]:
         print("Valor..: ", valores[indice])
         print("Serial.: ", seriais[indice])
+
+
+#situação 1
+depreciacao = input("Digite o nome do equipamento que será depreciado: ")
+
+for indice in range(0, len(equipamentos)):
+    if depreciacao == equipamentos[indice]:
+        print("Valor antigo: ", valores[indice])
+        valores[indice] = valores[indice] * 0.9
+        print("Novo valor: ", valores[indice])
+
+#situação 2
+serial = int(input("Digite o serial do equipamento que será excluido: "))
+
+for indice in range(0, len(departamentos)):
+    if seriais[indice] == serial:
+        del departamentos[indice]
+        del equipamentos[indice]
+        del seriais[indice]
+        del valores[indice]
+        break
+
+for indice in range(0, len(equipamentos)):
+    print("Equipamento..: ", (indice+1))
+    print("Nome.........: ", equipamentos[indice])
+    print("Valor........: ", valores[indice])
+    print("Serial.......: ", seriais[indice])
+    print("Departamento.: ", departamentos[indice])
