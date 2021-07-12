7a512be0c46d63692b0e405bbabedf8f47c727b2
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-12 19:04:30 -0300

    exemplo terminado

--- MeusProjetos_Python/Capitulo4_Dicionarios/Funcoes.py
+++ MeusProjetos_Python/Capitulo4_Dicionarios/Funcoes.py
@@ -11,3 +11,22 @@ def inserir(dicionario):
     dicionario[chave] = [input("Digite o nome: ").upper(),
                        input("Digite a última data de acesso: "),
                        input("Qual a última estação acessada: ").upper()]
+
+def pesquisar(chave, dicionario):
+    lista = dicionario.get(chave)
+    if lista != None:
+        print("Nome............: " + lista[0])
+        print("Último acesso ..: " + lista[1])
+        print("Última estação..: " + lista[2])
+
+def excluir(chave, dicionario):
+    if dicionario.get(chave) != None:
+        del dicionario[chave]
+    print("Objeto deletado")
+
+
+def listar(dicionario):
+    for chave, valor in dicionario.items():
+        print("Objeto......")
+        print("Login: ", chave)
+        print("Dados: ", valor)
