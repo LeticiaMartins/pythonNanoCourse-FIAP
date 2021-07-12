047aa3b0b8affc4691734452127789c1a75556df
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-12 18:51:12 -0300

    Versao com funcao

--- MeusProjetos_Python/Capitulo4_Dicionarios/ManagerUsers.py
+++ MeusProjetos_Python/Capitulo4_Dicionarios/ManagerUsers.py
@@ -1,19 +1,8 @@
+from Capitulo4_Dicionarios.Funcoes import *
 usuarios={}
-opcao = input("O que deseja realizar?"
-              + "<I> - Para Inserir um usuário"
-              + "<P> - Para Pesquisar um usuário"
-              + "<E> - Para Excluir um usuário"
-              + "<L> - Para Listar um usuário: ").upper()
+opcao = perguntar()
 
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
-        chave = input("Digite o login: ").upper()
-        nome = input("Digite o nome: ").upper()
-        data = input("Digite a última data de acesso: ")
-        estacao = input("Qua a última estação acessada: ").upper()
-        usuarios[chave] = [nome, data, estacao]
-    opcao = input("O que deseja realizasr?"
-                  + "<I> - Para Inserir um usuário"
-                  + "<P> - Para Pesquisar um usuário"
-                  + "<E> - Para Excluir um usuário"
-                  + "<L> - Para Listar um usuário: ").upper()
+        inserir(usuarios)
+    opcao = perguntar()
