2b16f1d46fcfae7f9c146d7d82afcc344e0522f6
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-14 18:15:29 -0300

    Uso do Write e Append

--- MeusProjetos_Python/Capitulo5_Manipula_Arquivos/GravarArquivo.py
+++ MeusProjetos_Python/Capitulo5_Manipula_Arquivos/GravarArquivo.py
@@ -1,4 +1,7 @@
 #with open("C:\MeusProjetos_Python\Capitulo5_Manipula_Arquivos\teste.txt", "r") as arquivo:
 
-with open("teste.txt", "r") as arquivo:
-    conteudo = arquivo.read()
+with open("teste.txt", "w") as arquivo:
+    arquivo.write("Nunca foi tão fácil criar um arquivo.")
+
+with open("teste.txt", "a") as arquivo:
+    arquivo.write("\nContinuação do texto.")
