a9bc14116768e925c1fab862b50436bb1e6804c1
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-14 20:27:53 -0300

    split

--- MeusProjetos_Python/Capitulo5_Manipula_Arquivos/Inventario.py
+++ MeusProjetos_Python/Capitulo5_Manipula_Arquivos/Inventario.py
@@ -10,12 +10,13 @@ while opcao>0 and opcao<4:
     elif opcao==3:
         resultado = exibir()
         for linha in resultado:
-            separacao = linha[linha.find(";")+1:-1]
-            data = separacao[0:separacao.find(";")]
-            separacao = separacao[separacao.find(";")+1:-1]
-            descricao = separacao[0:separacao.find(";")]
-            departamento = linha[linha.rfind(";")+1:-1]
-            print("Data........: ", data)
-            print("Descriçaõ...: ", descricao)
-            print("Departamento: ", departamento)
+            # separacao = linha[linha.find(";")+1:-1]
+            # data = separacao[0:separacao.find(";")]
+            # separacao = separacao[separacao.find(";")+1:-1]
+            # descricao = separacao[0:separacao.find(";")]
+            # departamento = linha[linha.rfind(";")+1:-1]
+            lista = linha.split(";")
+            print("Data........: ", lista[1])
+            print("Descriçaõ...: ", lista[2])
+            print("Departamento: ", lista[3])
     opcao = chamarMenu()
