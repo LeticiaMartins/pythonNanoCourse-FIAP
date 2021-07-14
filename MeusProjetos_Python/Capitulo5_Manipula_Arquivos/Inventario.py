5585006f510b7cff2c08778f61d82755b3632438
LeticiaMartins <leticiamartinsdev@gmail.com>
2021-07-14 20:11:50 -0300

    find e rfind

--- MeusProjetos_Python/Capitulo5_Manipula_Arquivos/Inventario.py
+++ MeusProjetos_Python/Capitulo5_Manipula_Arquivos/Inventario.py
@@ -8,5 +8,14 @@ while opcao>0 and opcao<4:
     elif opcao==2:
        persistir(inventario)
     elif opcao==3:
-        print(exibir())
+        resultado = exibir()
+        for linha in resultado:
+            separacao = linha[linha.find(";")+1:-1]
+            data = separacao[0:separacao.find(";")]
+            separacao = separacao[separacao.find(";")+1:-1]
+            descricao = separacao[0:separacao.find(";")]
+            departamento = linha[linha.rfind(";")+1:-1]
+            print("Data........: ", data)
+            print("Descriçaõ...: ", descricao)
+            print("Departamento: ", departamento)
     opcao = chamarMenu()
